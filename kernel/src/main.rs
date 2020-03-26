1dadb0ce7acde838ff649f0d07365b93a7f58a2a
Mrmaxmeier <Mrmaxmeier@gmail.com>
2020-03-26 21:47:19 +0100

    hook up net driver

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -21,6 +21,7 @@ mod debug;
 mod bare_bones;
 mod interrupts;
 mod memory;
+mod net;
 mod sched;
 mod snapshot;
 mod time;
@@ -60,6 +61,8 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         printk!("[    ] Memory ...\r");
     }
     memory::init(unsafe { &mut ALLOCATOR }, boot_info);
+    // remove stack range from allocatable memory
+    unsafe { memory::VirtualMemoryRegion::take_range(0x10000000000, 0x100003fffff) };
     printk!("[DONE] Memory    \n");
 
     // Set up interrupt/exception handling
@@ -68,20 +71,41 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     sched::user::init();
     printk!("[DONE] Interrupts    \n");
 
+
     // We can turn on interrupts now.
     x86_64::instructions::interrupts::enable();
 
-    printk!("[    ] Time ...\r");
-    let start = time::SysTime::now();
-    for target in 0..50 {
-        while time::SysTime::now() < start.after_ms(target * 20) {
-            x86_64::instructions::hlt();
+    if false {
+        printk!("[    ] Time ...\r");
+        let start = time::SysTime::now();
+        for target in 0..50 {
+            while time::SysTime::now() < start.after_ms(target * 20) {
+                x86_64::instructions::hlt();
+            }
+            printk!("[{:04}] Time ...\r", (target + 1) * 20);
         }
-        printk!("[{:04}] Time ...\r", (target + 1) * 20);
+        printk!("[DONE] Time    \n");
     }
-    printk!("[DONE] Time    \n");
 
-    let code = unsafe { VirtualMemoryRegion::take_range(0x13370000, 0x13370fff) };
+    for dev in tinypci::brute_force_scan() {
+        /*
+        printk!(
+            "PCI {:x}:{:x}: {:?}\n",
+            dev.vendor_id,
+            dev.device_id,
+            dev.full_class
+        );
+        */
+        if dev.vendor_id == 0x8086 && dev.device_id == 0x100e {
+            printk!("{}\n", dev);
+            net::setup_1000e(&dev);
+        }
+    }
+    printk!("[DONE] Enumerating PCI devices\n");
+
+    snapshot::init();
+
+    let code = unsafe { VirtualMemoryRegion::take_range(0x133713370000, 0x133713370fff) };
     {
         map_region(
             code.clone(),
@@ -93,8 +117,9 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         }
     }
     let stack = sched::user::allocate_user_stack();
+    printk!("userspace stack: {:#x?}\n", stack);
     printk!("[    ] Userspace ...\r");
     sched::user::start_user_task(code.start() as usize, stack);
-    // printk!("[DONE] Userspace    \n");
+    printk!("[DONE] Userspace    \n");
     // todo!();
 }
